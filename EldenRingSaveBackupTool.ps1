5e6b97ee543f492c09857748872dedc0417d1fdc
sario-tedesco <sariotedesco@gmail.com>
2022-10-19 16:45:27 +0200

    Fix -force directory configuration

--- EldenRingSaveBackupTool.ps1
+++ EldenRingSaveBackupTool.ps1
@@ -18,13 +18,36 @@ $backupSeamlessCoop = $true
 
 $backupVanilla = $true
 
+#Change if does crash or give errors about "$erSavePath = Join-Path -Path $erBasePath -ChildPath $userID"
+
+$Multyple_Directory = $false
+#After change the $Directory
+
+#Change $Directory to select the only one you need, this mod actualy crash/dont work if you do have more the 1 directory at C:\Users\<...>\AppData\Roaming\EldenRing
+
+$Directory = ""
+# example $Directory = "76561198246504570"
 
 #Don't change below code
 ###################################################################
 
 $delaySeconds = $delayMinutesBetweenBackups * 60
 $erBasePath = Join-Path -Path $env:APPDATA -ChildPath "\EldenRing"
-$userID = Get-ChildItem -Path $erBasePath -Name -Directory
+
+if($Multyple_Directory){
+	
+	<# Force directory#>
+	Write-Host "Forced directory selected."
+	$userID = Get-ChildItem -Path $erBasePath -Name -Directory -Filter $Directory
+	
+}else {
+	
+	<# Auto directory#>
+	Write-Host "Automatic directory selected."
+	$userID = Get-ChildItem -Path $erBasePath -Name -Directory
+	
+}
+
 $erSavePath = Join-Path -Path $erBasePath -ChildPath $userID
 $deletePathSeamless = [IO.Path]::Combine($backupDirectory, '*\', '*.co2')
 $deletePathVanilla = [IO.Path]::Combine($backupDirectory, '*\', '*.sl2')
